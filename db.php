f2dbebe667f2eaded47cc3af5ae54bb84b4d462b
Wahiba <165818251+Wahibased@users.noreply.github.com>
2024-11-21 12:58:56 +0100

    Update db.php

--- db.php
+++ db.php
@@ -20,4 +20,19 @@ try {
 } catch (PDOException $e) {
     die('Erreur : ' . $e->getMessage());
 }
+// Connexion MongoDB
+require 'vendor/autoload.php'; // Charger l'autoloader de Composer
+
+// Utiliser le namespace MongoDB
+use MongoDB\Client;
+
+// Configurer l'URI de connexion
+$uri = "mongodb+srv://wahi2024:wahi2024@cluster0.3les9.mongodb.net/calin_bebe";
+
+// Créer une instance de client MongoDB
+$client = new Client($uri);
+
+// Sélectionner la base de données et la collection
+$database = $client->selectDatabase('calin_bebe');  // Remplacer par le nom de votre base
+$collection = $database->selectCollection('avis'); // Remplacer par le nom de votre collection
 
