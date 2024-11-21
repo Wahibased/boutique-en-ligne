2175d396a3820a6518270e0757f705ae4fb351a3
Wahiba <165818251+Wahibased@users.noreply.github.com>
2024-11-21 11:11:59 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,12 @@
 <?php
-$host = 'localhost';
-$db = 'boutique';
-$user = 'root';
-$pass = '';
+$dbHost = 'c8lj070d5ubs83.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+$dbUser = 'ufjoi2ois2rh6h',
+$dbName = 'd73ivujlhosr3k',
+$dbPassword='pbcceb270079d474c77cd60d93c199d0ffdf4bf89a7399ffe50009bc6b89ad766',
+$dbPort = '5432',
+$dbPath ='',
+
+
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 $options = [
